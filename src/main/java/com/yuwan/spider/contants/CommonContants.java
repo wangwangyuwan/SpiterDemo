6765323c9872de543552ad773fd164969726b588
wangwangyuwan <muyouyuwano@163.com>
2019-07-15 23:25:05 +0800

    add sample to processor

---
+++ src/main/java/com/yuwan/spider/contants/CommonContants.java
@@ -0,0 +1,7 @@
+package com.yuwan.spider.contants;
+
+public class CommonContants {
+
+    public static final String ROOT_PATH = "https://www.jianshu.com";
+
+}
---
+++ src/main/java/com/yuwan/spider/controller/GithubRepoPageProcessor.java
@@ -0,0 +1,18 @@
+package com.yuwan.spider.controller;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class GithubRepoPageProcessor implements PageProcessor {
+
+    @Override
+    public void process(Page page) {
+
+    }
+
+    @Override
+    public Site getSite() {
+        return null;
+    }
+}
